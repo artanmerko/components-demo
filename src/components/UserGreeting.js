1fd03ce8939812ad80b040d7e6d8bda47e912666
Artan Merko <97398977+artanmerko@users.noreply.github.com>
2022-07-22 10:05:25 -0400

    Update UserGreeting.js

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -9,38 +9,8 @@ class UserGreeting extends Component {
     }
   }
 
-  // #if-else approach
-  // render() {
-  //   if (this.state.isLoggedIn) {
-  //     return <div>Welcome Vishwas</div>
-  //   } else {
-  //     return <div>Welcome Guest</div>
-  //   }
-  // }
-
-  // #element-variables approach
-  // render() {
-  //   let message
-  //   if (this.state.isLoggedIn) {
-  //     message = <div>Welcome Vishwas</div>
-  //   } else {
-  //     message = <div>Welcome Guest</div>
-  //   }
-  //   return <div>{message}</div>
-  // }
-
-  // #ternary-operator-approach
-  // render() {
-  //   return this.state.isLoggedIn ? (
-  //     <div>Welcome Vishwas</div>
-  //   ) : (
-  //     <div>Welcome Guest</div>
-  //   )
-  // }
-
-  // #short-circuit-operator-approach
   render() {
-    return this.state.isLoggedIn && <div>Welcome Vishwas</div>
+    return this.state.isLoggedIn && <div>Welcome </div>
   }
 }
 
